726c41bf82d7a98d85cd5dc09d41c8fcb1fd28c8
Dave6666 <905346588@qq.com>
2025-10-04 19:09:32 +0800

    TubeConnect

---
+++ DD/TubeConnect/CreateMD5_DD.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001
+echo *********************
+echo CreateMD5
+echo *********************
+python ../../CreateMD5.py
+
+
+
+echo .
+echo 已执行完毕(退出请按任意键或直接关闭窗体)-----------------------
+echo .
+pause
