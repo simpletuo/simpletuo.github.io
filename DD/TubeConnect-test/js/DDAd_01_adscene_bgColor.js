8c5232791758cf75f1ecbc0f0d8ea518927fcb2d
Dave6666 <905346588@qq.com>
2025-10-05 18:50:09 +0800

    TubeConnect

---
+++ DD/TubeConnect-test/js/DDAd_01_adscene_bgColor.js
@@ -0,0 +1,328 @@
+var times_bottom_banner_ad = 0;
+function appendAsenseAD(obj,_ins){if((typeof obj=="undefined") || (typeof _ins =="undefined")){return;}obj.innerHTML=_ins;let o=document.createElement("script");o.innerHTML="(adsbygoogle = window.adsbygoogle || []).push({});";obj.appendChild(o);}
+function creatBottomBannerBox(){let _container=document.getElementById('bannerBox');if(_container==null){times_bottom_banner_ad = 0;let bannerBox=document.getElementById('bannerBox');if((typeof bannerBox) == "undefined" || bannerBox == null){bannerBox=document.createElement('Div');bannerBox.id="bannerBox";document.body.appendChild(bannerBox);} bannerBox.innerHTML='	<div id="bottom-banner-container" style="background-color:#fff;pointer-events:none; bottom: 0px; left: 0;  padding-top: 2px;  padding-bottom: 0px;  position: fixed;  visibility: visible; width: 100%; z-index: 19; "><div id="bottom-banner" align="center" style="overflow: hidden;margin: 0 auto;width: 100%;height: 100px;"></div></div>';}};
+function RemoveBottomBannerBox(){let bannerBox = document.getElementById('bannerBox');if(bannerBox!=null){bannerBox.remove();}}
+function ShowBottomContainer(){let _element= document.getElementById('bottom-banner-container');if(_element!=null){return};_element.style.backgroundColor="#fff";_element.style.boxShadow="0 0 5px 0 rgb(0 0 0 / 20%)"; _element.style.pointerEvents = 'auto';}
+function show_banner(){creatBottomBannerBox();let _banner=document.getElementById('bottom-banner');_banner.innerHTML = "";appendAsenseAD(_banner,bottom_banner_Ad);setTimeout(check_banner,1000);if(typeof is_fresh_bottom_banner!="undefined"&&is_fresh_bottom_banner==true){setTimeout(show_banner,time_fresh_banner);} }
+function reshow_banner(){RemoveBottomBannerBox();setTimeout(show_banner,6200)}
+function check_banner(){let _banner=document.getElementById('bottom_banner_Ad'); if(_banner==undefined){return;}  var attribute = _banner.getAttribute("data-ad-status");if(attribute==null){times_bottom_banner_ad+=1;if(times_bottom_banner_ad==1){ShowBottomContainer();}if(times_bottom_banner_ad >=8){reshow_banner(); return;} setTimeout(check_banner,1000);return;} if(attribute=="unfilled"){reshow_banner(); return;}  }
+function Yuan_startGame(){adsenseAnchorRemove();CheckGoogleADS();show_banner();if(typeof is_show_top_banner=="undefined"||is_show_top_banner==true){setTimeout(creat_topAnr,3500);}}
+function adsenseAnchorRemove(){var Yuan_adID=document.getElementsByTagName('ins');for(var i=0;i<Yuan_adID.length;i++){if(Yuan_adID[i].getAttribute("data-anchor-status")){Yuan_adID[i].remove()}}}
+var time_secends_ShowAnrAd = 0; let timeoutId_ShowTopAnr = -1;
+function ShowAnrAd(){ time_secends_ShowAnrAd += 1;let elementTopBanner= document.getElementById('top-banner-container');let elementTop_banner_Ad = document.getElementById('top_banner_Ad');if(time_secends_ShowAnrAd >= 10 || elementTopBanner==null || elementTop_banner_Ad==null){creat_topAnr();return;}var attribute = elementTop_banner_Ad.getAttribute("data-ad-status");if(attribute==null){setTimeout(ShowAnrAd,1000);return;} if(attribute=="unfilled"){topAnrRemove(); return;} elementTopBanner.style.backgroundColor="#fff";elementTopBanner.style.boxShadow="0 0 5px 0 rgb(0 0 0 / 20%)"; elementTopBanner.style.pointerEvents = 'auto'; setTimeout(ShowAnrAd_CloseBtn,3000);RemoveTimeoutId_ShowTopAnr();timeoutId_ShowTopAnr=setTimeout(topAnrRemoveShowLater,time_fresh_top_banner);}
+function ShowAnrAd_CloseBtn(){ let btnClose = document.getElementById('top-banner-close');if(btnClose==null){return;}btnClose.style.display='block';  btnClose.pointerEvents = "auto";}
+function creat_topAnr(){RemoveTimeoutId_ShowTopAnr();time_secends_ShowAnrAd = 0;adsenseAnchorRemove();if(!navigator.onLine){topAnrRemoveShowLater();return;} let topBannerBox=document.getElementById('topBannerBox');if((typeof topBannerBox) == "undefined" || topBannerBox == null){topBannerBox=document.createElement('Div');topBannerBox.id="topBannerBox";document.body.appendChild(topBannerBox);} topBannerBox.innerHTML='<div id="top-banner-container" style="background-color:#fff;pointer-events:none; top: 0px; left: 0;  padding-top: 3px;  padding-bottom: 2px;  position: fixed;  visibility: visible; width: 100%; z-index: 19; "><div id="top-banner-close" style="display: none;bottom: 0;position: relative;text-align: right;width: 100%;box-sizing: border-box;"><button id="top-banner-close-button" onclick="topAnrRemoveDeley()" style="background-color: #fff; border-style: none; border-radius:0 0 8px 8px;box-shadow: 0 -1px 1px 0 rgb(0 0 0 / 20%); outline: 0; position: absolute;left: 0; top: 100px; width: 28px;height: 20px;">X</button></div><div id="top-banner" align="center" style="overflow: hidden;margin: 0 auto;width: 100%;height: 100px;"></div></div>';appendAsenseAD(document.getElementById('top-banner'),top_banner_Ad);setTimeout(ShowAnrAd,1000);}
+function topAnrRemove(){ RemoveAndShowLater(time_fresh_top_banner); }
+function topAnrRemoveShowLater(){ RemoveAndShowLater(50); }
+function topAnrRemoveNoShowAgain(){let topBannerBox = document.getElementById('topBannerBox');if(topBannerBox!=null){topBannerBox.remove();}}
+function RemoveAndShowLater(time){let topBannerBox = document.getElementById('topBannerBox');if(topBannerBox!=null){topBannerBox.remove();} RemoveTimeoutId_ShowTopAnr();timeoutId_ShowTopAnr = setTimeout(creat_topAnr,time);}
+function topAnrRemoveDeley(){setTimeout(topAnrRemove,1000);}
+function RemoveTimeoutId_ShowTopAnr(){clearTimeout(timeoutId_ShowTopAnr);}
+function CheckGoogleADS()
+{	
+	if(window.adsbygoogle!=undefined && window.adsbygoogle["loaded"] == true)return;
+	if(google_adScene_js!=undefined && navigator.onLine)
+	{
+		let script = document.createElement("script");
+		script.async = true;//"async"
+		script.src = google_adScene_js;
+		//document.body.appendChild(script);
+		document.head.appendChild(script);
+	}
+	setTimeout(CheckGoogleADS,5000);
+}
+
+var htmlUrlModeAfc1 = './ShowAD_8.html';
+var timeIntModeAfc1 = 8;
+var timeRwModeAfc1 = 10;
+var timeADNextLevel = 8;
+let isAD_Award = false;
+var ad_callbackName = null;
+var adType = null;
+var callbackOnViewedAD = null;
+var insertADCount = 3;//插页广告位数量
+var insertIndex = 1;//当前插页广告的index
+var countLoadingadsafcIntMode1 = 0;
+let isADNoTimeOut = !1;
+
+function callAfcIntMode1() {
+    if ($('#ad_position_box')) {
+        $('#ad_position_box').remove()
+    }
+	countLoadingadsafcIntMode1 = 0;
+    var ad_position_box = document.createElement("DIV");
+    ad_position_box.id = "ad_position_box";
+    ad_position_box.style.width = "100%",
+    ad_position_box.style.height = "200%",
+    ad_position_box.style.position = "absolute",
+    ad_position_box.style.top = "0",
+    ad_position_box.style.zIndex = 1500,
+    ad_position_box.style.left = "0";
+    document.getElementsByTagName("body")[0].appendChild(ad_position_box);
+    $("#ad_position_box").load(htmlUrlModeAfc1, function(responseTxt, statusTxt, xhr) {
+        $("#adsense_position_box").css({
+            "background": "rgba(52, 58, 65, 0.6)",
+            "backdrop-filter": "blur(5px)"
+        });
+		if(isADNoTimeOut){
+			loadingadsafcIntMode1(0);
+		}else if(adType=="next"){
+			loadingadsafcIntMode1(timeADNextLevel);
+		}else{
+			loadingadsafcIntMode1(timeIntModeAfc1);
+		}
+
+        
+       
+		/*
+		if(adType=="pause")//暂停广告 
+		{
+			 appendAsenseAD(document.getElementById('creative'), insert_Ad_Pause);
+			
+		}else//其他插页广告  三个插页广告轮流播放*/
+		{	
+			if(insertIndex > insertADCount)
+			{
+				insertIndex = 1;
+			}
+			
+			let _insert_Ad = insert_Ad_1;
+			
+			if(insertIndex == 2)
+			{	
+				if((typeof insert_Ad_2) != "undefined")
+				{
+					_insert_Ad = insert_Ad_2;
+				}
+				
+			}else if(insertIndex == 3)
+			{	
+		
+				if((typeof insert_Ad_3) != "undefined")
+				{
+					_insert_Ad = insert_Ad_3;
+				}
+				
+			}
+			 appendAsenseAD(document.getElementById('creative'), _insert_Ad);
+			 
+			 insertIndex += 1;
+			
+		}
+		
+		if(typeof bottom_AD_250 != "undefined")
+		{
+		
+			appendAsenseAD(document.getElementById('creative2'), bottom_AD_250);
+		}
+		
+    });
+}
+
+var awardADCount = 2;//
+var awardADIndex = 1;//
+function callAfcRwMode1() {
+    if ($('#ad_position_box')) {
+        $('#ad_position_box').remove()
+    }
+	countLoadingadsafcIntMode1 = 0;
+    var ad_position_box = document.createElement("DIV");
+    ad_position_box.id = "ad_position_box";
+    ad_position_box.style.width = "100%",
+    ad_position_box.style.height = "135%",
+    ad_position_box.style.position = "absolute",
+    ad_position_box.style.top = "0",
+    ad_position_box.style.zIndex = 1500,
+    ad_position_box.style.left = "0";
+    document.getElementsByTagName("body")[0].appendChild(ad_position_box);
+    $("#ad_position_box").load(htmlUrlModeAfc1, function(responseTxt, statusTxt, xhr) {
+        $('#adsense_position_box').css({
+            "background": "rgba(52, 58, 65, 0.6)",
+            "backdrop-filter": "blur(5px)"
+        });
+        adsafcloadingRwMode1(timeRwModeAfc1);
+		
+		if(awardADIndex > awardADCount)
+		{
+			awardADIndex = 1;
+		}
+			
+		let _award_AD = award_AD_1;
+			
+		if(awardADIndex == 2)
+		{	
+	
+			if((typeof award_AD_2) != "undefined")
+			{
+				_award_AD = award_AD_2;
+			}	
+		}
+		
+		if(ad_callbackName=="GetCarADAward")
+		{
+			if((typeof award_AD_Car) != "undefined")
+			{
+				_award_AD = award_AD_Car;
+			}
+		}
+			
+        appendAsenseAD(document.getElementById('creative'), _award_AD);
+		if(typeof bottom_AD_250 != "undefined")
+		{
+			appendAsenseAD(document.getElementById('creative2'), bottom_AD_250);
+		}
+		
+		awardADIndex += 1;
+
+    });
+}
+
+
+var hasClick = false;
+function loadingadsafcIntMode1(_i) {
+
+	if(countLoadingadsafcIntMode1 == 6)//第6秒显示关闭按钮
+	{
+		$('#dismiss-button').css({"visibility": "visible"});
+	}
+	countLoadingadsafcIntMode1 += 1;
+	
+    _i = _i - 1;
+    if (_i > 0) {
+       // $('#count-down-text').html("Loading in " + _i + " seconds");
+        setTimeout('loadingadsafcIntMode1(' + _i + ')', 1000);
+    } else {
+		hasClick = true;
+		countLoadingadsafcIntMode1 = 0;
+		
+		if(isADNoTimeOut)
+		{
+		  isADNoTimeOut=!1;
+		  $('#dismiss-button').css({"visibility": "visible"});
+		  $('#dismiss-button').on('click',moveAdsAfcRwMode1);
+		}else
+		{
+			let closeTime = RandomNumber(1000,1500);
+			setTimeout(moveAdsAfcRwMode1,closeTime);
+		}
+    }
+}
+
+function RandomNumber(min,max)
+{	
+	max = max - min;
+	min = min;
+	return Math.floor((Math.random() * max) + min);
+}
+
+function adsafcloadingRwMode1(_i) {
+	
+	if(countLoadingadsafcIntMode1 == 8)//第6秒显示关闭按钮
+	{
+		$('#dismiss-button').css({"visibility": "visible"});
+	}
+	countLoadingadsafcIntMode1 += 1;
+	
+    _i = _i - 1;
+    if (_i > 0) {
+        //$('#count-down-text').html("Reward in " + _i + " seconds");
+        setTimeout('adsafcloadingRwMode1(' + _i + ')', 1000);
+    } else {
+		countLoadingadsafcIntMode1 = 0;
+		let closeTime = RandomNumber(1000,1500);
+		setTimeout(moveAdsAfcRwMode1,closeTime);
+    }
+}
+function moveAdsAfcRwMode1() {
+    $('#ad_position_box').remove();
+	if(adType!="pause")
+	{
+		if(typeof ResumeGame == "function")
+		{
+			ResumeGame();
+		}
+		if(ad_callbackName!=null&&MyUnityInstance!=null)
+		{	
+			if(typeof adObjName == "undefined")
+			{
+				adObjName = "GameManager";
+			}
+				
+			if(typeof adRewardName=="undefined" || null == adRewardName)
+			{
+				
+				MyUnityInstance.SendMessage(adObjName,ad_callbackName);
+			}
+			else 
+			{
+				
+				MyUnityInstance.SendMessage(adObjName,ad_callbackName,adRewardName);
+				
+			}
+			
+		}
+	}
+	if(callbackOnViewedAD!=null)
+	{	
+		callbackOnViewedAD();
+	}
+
+}
+
+function ShowADStart_NoTimeOut()
+{	
+	isADNoTimeOut = !0;
+	let adName ="browse";
+	ShowAD(false,null,adName,null,adName);
+}
+
+function ShowAD(isReward,callbackName,_adType,_objName,_adRewardName)
+{	
+	if (!isReward&&IsOnShowAD())return;
+	if(typeof PauseGame == "function")
+	{
+		PauseGame();
+	}	
+	isAD_Award = isReward;
+	ad_callbackName = callbackName;
+	adType = _adType;
+	adObjName = _objName;
+	adRewardName = _adRewardName;
+	if(isReward)
+	{	
+		
+		callAfcRwMode1();
+	}else
+	{
+		callAfcIntMode1();
+	}
+	
+}
+function IsOnShowAD()
+{
+	let ad_position_box = document.getElementById("ad_position_box");
+	return ad_position_box != undefined;
+}
+
+function ShowADForLayaGame(isReward,_adType,_callbackOnViewedAD)
+{	
+	if (!isReward&&IsOnShowAD())return;
+
+	if(typeof PauseGame == "function")
+	{
+		PauseGame();
+	}
+	isAD_Award = isReward;
+	callbackOnViewedAD = _callbackOnViewedAD;
+	ad_callbackName = null;
+	adType = _adType;
+	if(isReward)
+	{	
+		
+		callAfcRwMode1();
+	}else
+	{
+		callAfcIntMode1();
+	}
+}
+//Yuan_startGame(); 
