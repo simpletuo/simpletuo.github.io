0a0cd73a663cea6177762dc2531b9503b4e79e8a
dave66 <905346588@qq.com>
2026-01-05 10:56:11 +0800

    game

---
+++ res/game32/web-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","rope","wall"],collisionMatrix:[[false],[false,false,true],[false,true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/mainScene.fire",orientation:"portrait",jsList:[]};
