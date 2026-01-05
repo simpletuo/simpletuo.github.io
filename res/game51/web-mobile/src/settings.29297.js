4d10a17d9c1daaccf8695313fd03da7e541466a1
dave66 <905346588@qq.com>
2026-01-05 11:02:21 +0800

    game

---
+++ res/game51/web-mobile/src/settings.29297.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/block_loading.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"52af2"}};
