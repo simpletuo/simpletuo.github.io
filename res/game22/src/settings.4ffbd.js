c852cda0b590c637e825e61efd16f8cec1d929fc
dave66 <905346588@qq.com>
2026-01-05 10:52:58 +0800

    game

---
+++ res/game22/src/settings.4ffbd.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/GF/StartScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"8b89d",main:"547d8"}};
